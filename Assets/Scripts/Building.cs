1af6f5906520953d3a03e78a330fa42f4f1022e4
Aatir Khan <aatirk99@hotmail.com>
2023-01-31 01:28:01 +0500

    Incorporated SO for Panel
    Used Scriptable Objects to update panel with bio details of clicked department.

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Building", menuName = "Building")]
+public class Building : ScriptableObject
+{
+    public new string name;
+    public string description;
+}
--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
-
     public void OpenPanel()
     {
-        Debug.Log("pls");
         if (Panel != null)
         {
             Panel.SetActive(true);
